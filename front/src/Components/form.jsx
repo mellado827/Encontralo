f100790a51489339d3db046dd816c316d6eafadd
Nicolás <mellado827@gmail.com>
2020-09-27 17:33:48 -0300

    Contact Form Component done (emailJS)

--- front/src/Components/form.jsx
+++ front/src/Components/form.jsx
@@ -1,58 +1,94 @@
 import React from 'react'
+import emailjs from 'emailjs-com'
+import Swal from 'sweetalert2'
 import Navbar from '../Components/navbar'
+import { init } from 'emailjs-com';
+init("user_mOJKehHBkb7ul8DMSXNQF");
 
-class Form extends React.Component {
+export default function Form(props) {
 
-    constructor(props) {
-        super()
+    document.title = "Encontralo / formulario "
+
+    function sendEmail(e) {
+        e.preventDefault();
+
+        emailjs.sendForm('encontralo', 'template_yf2moyf', e.target, 'user_mOJKehHBkb7ul8DMSXNQF')
+            .then(() => {
+                Swal.fire({
+                    icon: 'success',
+                    title: '¡Formulario enviado satisfactoriamente!',
+                    text: 'El equipo de Encontralo lo estará revisando a la brevedad.',
+                    customClass: {
+                        content: 'text_fontstyle'
+                    }
+                })
+                setTimeout(() => {
+                    props.history.push('/')
+                }, 3000);
+            }, () => {
+                Swal.fire({
+                    icon: 'error',
+                    title: 'Ups!',
+                    text: 'Hubo un error, inténtalo de nuevo más tarde',
+                    customClass: {
+                        content: 'text_fontstyle'
+                    }
+                })
+            });
     }
 
-    componentDidMount() {
-        document.title = "Encontralo - Formulario"
-    }
+    return (
+        <>
+            <Navbar />
+            <div className="report d-flex flex-column justify-content-center align-items-center">
 
-    render() {
-        return (
-            <>
-                <Navbar />
-                <div className="report d-flex flex-column justify-content-center align-items-center">
+                <div className="report_title mt-5">
+                    <h1 className="text-center subtitle_fontstyle report_title">Formulario</h1>
+                    <p className="text-center text_fontstyle">Si quieres unirte a Encontralo, brindar alguna idea o demás, ¡Estamos abiertos! Don't be shy.</p>
+                </div>
 
-                    <div className="report_title mt-5">
-                        <h1 className="text-center subtitle_fontstyle report_title">Formulario</h1>
-                        <p className="text-center text_fontstyle">Si quieres unirte a Encontralo, brindar alguna idea o demás, ¡Estamos abiertos! Don't be shy.</p>
+                <form className="form d-flex flex-column justify-content-start" onSubmit={sendEmail}>
+                    <div className="d-flex flex-column align-items-center">
+                        <div className="form-group m-3 pt-3 input_sizeForm">
+                            <label htmlFor="formGroupExampleInput" className="text_fontstyle"><u>Nombre</u> *</label>
+                            <input type="text"
+                                className="form-control text_fontstyle"
+                                id="formGroupExampleInput"
+                                name="name"
+                                placeholder="Ingresa tu nombre" />
+                        </div>
+                        <div className="form-group m-3 pt-3 input_sizeForm">
+                            <label htmlFor="exampleInputEmail1" className="text_fontstyle"><u>Correo electrónico</u> *</label>
+                            <input type="email"
+                                className="form-control text_fontstyle"
+                                id="exampleInputEmail1"
+                                aria-describedby="emailHelp"
+                                name="email"
+                                placeholder="Ingresa tu correo" />
+                        </div>
+                        <div className="form-group m-3 pt-3 input_sizeForm">
+                            <label htmlFor="formGroupExampleInput" className="text_fontstyle"><u>Asunto</u> *</label>
+                            <input type="text"
+                                name="subject"
+                                className="form-control text_fontstyle"
+                                id="formGroupExampleInput"
+                                placeholder="Ingresa el asunto" />
+                        </div>
+                        <div className="form-group m-3 pt-3 input_sizeForm">
+                            <label htmlFor="exampleFormControlTextarea1" className="text_fontstyle"><u>Mensaje</u> *</label>
+                            <textarea className="form-control text_fontstyle"
+                                name="message"
+                                id="exampleFormControlTextarea1"
+                                rows="8"></textarea>
+                        </div>
+                        <button type="submit"
+                            className="mt-3 cta_bottonsstyle text_fontstyle mb-3">Enviar
+                        </button>
                     </div>
 
-                    <form className="form d-flex flex-column justify-content-start">
-                        <div className="d-flex flex-column align-items-center">
-                            <div className="form-group m-3 pt-3 input_sizeForm">
-                                <label htmlFor="formGroupExampleInput" className="text_fontstyle"><u>Nombre</u> *</label>
-                                <input type="text" className="form-control  text_fontstyle" id="formGroupExampleInput" placeholder="Ingresa tu nombre" />
-                            </div>
-                            <div className="form-group m-3 pt-3 input_sizeForm">
-                                <label htmlFor="formGroupExampleInput" className="text_fontstyle"><u>Apellido</u> *</label>
-                                <input type="text" className="form-control text_fontstyle " id="formGroupExampleInput" placeholder="Ingresa tu apellido" />
-                            </div>
-                            <div className="form-group m-3 pt-3 input_sizeForm">
-                                <label htmlFor="exampleInputEmail1" className="text_fontstyle"><u>Correo electrónico</u> *</label>
-                                <input type="email" className="form-control text_fontstyle" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Ingresa tu correo" />
-                            </div>
-                            <div className="form-group m-3 pt-3 input_sizeForm">
-                                <label htmlFor="formGroupExampleInput" className="text_fontstyle"><u>Asunto</u> *</label>
-                                <input type="text" className="form-control  text_fontstyle" id="formGroupExampleInput" placeholder="Ingresa el asunto" />
-                            </div>
-                            <div className="form-group m-3 pt-3 input_sizeForm">
-                                <label htmlFor="exampleFormControlTextarea1" className="text_fontstyle"><u>Mensaje</u> *</label>
-                                <textarea className="form-control text_fontstyle" id="exampleFormControlTextarea1" rows="8"></textarea>
-                            </div>
-                            <button type="submit" className="mt-3 cta_bottonsstyle text_fontstyle mb-3">Enviar</button>
-                        </div>
-
-                    </form>
-                </div>
-            </>
-        )
-    }
-
+                </form>
+            </div>
+        </>
+    )
 }
 
-export default Form
