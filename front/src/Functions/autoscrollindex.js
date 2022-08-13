3077c82031491782c13a28b25980aee681d56c55
mellado827 <mellado827@gmail.com>
2022-08-13 00:14:53 -0300

    Just front.

--- front/src/Functions/autoscrollindex.js
+++ front/src/Functions/autoscrollindex.js
@@ -1,12 +1,12 @@
 import $ from 'jquery'
 
 function scroll() {
-  setTimeout(function () {
-    window.scrollTo({
-      top: $("#calltoaction").offset().top,
-      behavior: "smooth",
-    });
-  }, 3000);
+  // setTimeout(function () {
+  //   window.scrollTo({
+  //     top: $("#calltoaction").offset().top,
+  //     behavior: "smooth",
+  //   });
+  // }, 3000);
 }
 
 export default scroll
